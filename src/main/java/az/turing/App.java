f86ed3e38df80bd1e9d5822c829d1aa106760df3
ayten-github <ababakishiyeva@std.beu.edu.az>
2024-11-01 19:04:54 +0400

    initial commit

---
+++ src/main/java/az/turing/App.java
@@ -0,0 +1,14 @@
+package az.turing;
+
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+
+    }
+}
